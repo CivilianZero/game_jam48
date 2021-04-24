7dc5bfdcd08c874f0006632ce582f998a0a9fc1e
Kevin Smith <civilianzero@icloud.com>
2021-04-23 23:57:02 -0400

    initial commit

---
+++ main/sub.go
@@ -0,0 +1,15 @@
+components {
+  id: "sub"
+  component: "/main/sub.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

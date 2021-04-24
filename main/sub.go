0ad24ac65a0b443d95bcb406309f3563c23a724f
Jordan Carroll <jfindlayc@gmail.com>
2021-04-25 00:58:37 +0900

    Add vision cone

--- main/sub.go
+++ main/sub.go
@@ -98,3 +98,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "vision cone"
+  type: "sprite"
+  data: "tile_set: \"/main/sprites.atlas\"\n"
+  "default_animation: \"vision cone\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 4.0
+    y: -7.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

31bcc6049e17a5b417312f1f7c7aa45953c74382
Kevin Smith <civilianzero@icloud.com>
2021-04-24 03:30:02 -0400

    torpedo stuff

--- main/sub.go
+++ main/sub.go
@@ -33,3 +33,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "torpedofactory"
+  type: "factory"
+  data: "prototype: \"/main/torpedo.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

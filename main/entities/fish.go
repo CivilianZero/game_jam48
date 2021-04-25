82eb480d45d00e8c649aeaf87f2a8c51c97cebfe
Kevin Smith <civilianzero@icloud.com>
2021-04-25 01:00:54 -0400

    basic pathfinding works now

--- main/entities/fish.go
+++ main/entities/fish.go
@@ -1,6 +1,6 @@
 components {
-  id: "sub"
-  component: "/main/entities/sub.script"
+  id: "fish"
+  component: "/main/entities/fish.script"
   position {
     x: 0.0
     y: 0.0
@@ -17,27 +17,9 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/sprites.atlas\"\n"
-  "default_animation: \"submarine\"\n"
+  "default_animation: \"anglerfish\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "torpedofactory"
-  type: "factory"
-  data: "prototype: \"/main/entities/torpedo.go\"\n"
-  "load_dynamically: false\n"
   ""
   position {
     x: 0.0
@@ -59,15 +41,15 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"sub\"\n"
-  "mask: \"cave\"\n"
-  "mask: \"enemy\"\n"
+  "group: \"enemy\"\n"
+  "mask: \"sub\"\n"
+  "mask: \"torpedo\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
+  "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: -1.0\n"
-  "      y: 1.0\n"
+  "      x: -5.0\n"
+  "      y: -2.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -77,40 +59,37 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 12.0\n"
+  "      y: 1.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 1\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 9.097\n"
-  "  data: 6.283\n"
+  "  data: 13.687\n"
+  "  data: 10.0\n"
+  "  data: 7.455\n"
   "  data: 10.0\n"
   "}\n"
-  "linear_damping: 50.0\n"
-  "angular_damping: 50.0\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
   "locked_rotation: true\n"
   ""
   position {
     x: 0.0
     y: 0.0
     z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "vision_cone"
-  type: "sprite"
-  data: "tile_set: \"/main/sprites.atlas\"\n"
-  "default_animation: \"vision cone\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 4.0
-    y: -7.0
-    z: 1.0
   }
   rotation {
     x: 0.0

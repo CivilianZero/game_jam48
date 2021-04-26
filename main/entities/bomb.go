5da441f6e76758e1abd83510f1cfc842345e4a65
Kevin Smith <civilianzero@icloud.com>
2021-04-25 23:00:51 -0400

    bomb stuff

--- main/entities/bomb.go
+++ main/entities/bomb.go
@@ -78,3 +78,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "lockon"
+  type: "sprite"
+  data: "tile_set: \"/main/sprites.atlas\"\n"
+  "default_animation: \"empty\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 52.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
